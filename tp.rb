66ab788c6dc1551f3ee58931c3e1e6723a89ee4b
Eli Front <elifront23@gmail.com>
2024-04-21 21:18:49 -0500

    update rb

--- tp.rb
+++ tp.rb
@@ -1,7 +1,7 @@
 class TP < Formula
   desc "Teleport"
   homepage "https://github.com/eli-front/tp"
-  url "https://github.com/eli-front/tp/archive/v1.0.0.tar.gz"
+  url "https://github.com/eli-front/tp/releases/tag/v0.1.0"
   sha256 "6a1be5ba87289e1b7367abec75175fd4a7e6b7083e1267fdb36b59eb228eb0ae"
 
   def install
